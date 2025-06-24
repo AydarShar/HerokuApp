63019295b1b4889b6539e1e29e4f9956f67f880e
Aydar Sharafetdin <aydar141189@gmail.com>
2025-06-24 09:39:54 +0300

    added seleniumBasic tests

---
+++ src/test/java/CheckboxesTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class CheckboxesTest {
+
+    @Test
+    public void checkboxesTest() {
+
+        WebDriver driver = new ChromeDriver();
+        SoftAssert softAssert = new SoftAssert();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        WebElement firstCheckbox = driver.findElement(By.xpath("//form[@id=\"checkboxes\"]/input[1]"));
+        WebElement secondCheckbox = driver.findElement(By.xpath("//form[@id=\"checkboxes\"]/input[2]"));
+        // проверили первый чекбокс
+        softAssert.assertFalse(firstCheckbox.isSelected());
+        firstCheckbox.click();
+        softAssert.assertTrue(firstCheckbox.isSelected());
+        // проверили второй чекбокс
+        softAssert.assertTrue(secondCheckbox.isSelected());
+        secondCheckbox.click();
+        softAssert.assertFalse(secondCheckbox.isSelected());
+        driver.quit();
+        softAssert.assertAll();
+    }
+}
---
+++ src/test/java/NotificationMessagesTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class NotificationMessagesTest {
+
+    @Test
+    public void notificationMessagesTest() {
+
+        WebDriver driver = new ChromeDriver();
+        SoftAssert softAssert = new SoftAssert();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://the-internet.herokuapp.com/notification_message_rendered");
+        driver.findElement(By.xpath("//a[contains(text(), 'Click here')]")).click();
+        softAssert.assertEquals(
+                driver.findElement(By.xpath("//div[@id=\"flash\"]")).getText(),
+                "Action unsuccesful, please try again\n" +
+                        "×");
+        driver.quit();
+        softAssert.assertAll();
+    }
+}
