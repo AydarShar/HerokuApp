272248db72a3954f4a5e17eea2f99460bf0c0b26
Aydar Sharafetdin <aydar141189@gmail.com>
2025-07-06 13:04:54 +0300

    added seleniumAdvanced tests

--- src/test/java/seleniumBasic/TyposTest.java
+++ src/test/java/seleniumBasic/TyposTest.java
@@ -1,10 +1,9 @@
+package seleniumBasic;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
